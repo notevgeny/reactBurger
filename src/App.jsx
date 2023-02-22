9de0201874990e831b1402f4d2a5e7798a0ba898
notevgeny <gcause17@gmail.com>
2023-02-22 14:45:08 +0300

    added useState, RTK (CategorySlice)

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Navigation } from './components/Navigation/Navigation'
 import { Catalog } from './components/Catalog/Catalog'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 export const App = () => {
 
   return (
-    <>
+    <Provider store={store}>
       <Header/>
       <main>
          <Navigation/>
          <Catalog />
       </main>
       <Footer/>
-    </>
+    </Provider>
       
   )
 }
--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import style from './count.module.css';
 
-export const Count = ({count}) => {
+export const Count = (props) => {
+
+  const [count, setCount] = useState(props.count);
+
+  const plusOne = () => {  
+    setCount(count + 1);
+  }
+
+  const minusOne = () => {
+    if (count > 1){
+      setCount(count - 1);
+    }
+  }
+
   return (
     <div className={style.count}>
-      <button className={style.minus}>-</button>
+      <button 
+        className={style.minus}
+        onClick={minusOne}
+        disabled={count === 1}
+      >-</button>
       <p className={style.amount}>{count}</p>
-      <button className={style.plus}>+</button>
+      <button 
+        className={style.plus}
+        onClick={plusOne}
+      >+</button>
     </div>
   );
 };
