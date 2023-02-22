ed63e1b0b4dd843c3500c5aa14994c86d5b5553b
notevgeny <gcause17@gmail.com>
2023-02-22 13:42:54 +0300

    separated Catalog, Count, Order, OrderGoods with styles

---
+++ src/components/Order/OrderGoods/OrderGoods.jsx
@@ -0,0 +1,23 @@
+import { Count } from '../../Count/Count';
+import style from './orderGoods.module.css';
+
+export const OrderGoods = ({title}) => {
+  return (
+    <li className={style.item}>
+      <img className={style.image} src="img/burger_1.jpg" alt={title}/>
+
+      <div className={style.goods}>
+        <h3 className={style.title}>{title}</h3>
+
+        <p className={style.weight}>512г</p>
+
+        <p className={style.price}>
+          1279
+          <span className="currency">₽</span>
+        </p>
+      </div>
+
+      <Count count={2}/>
+    </li>
+  );
+};
